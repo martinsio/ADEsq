f093eb24efb5761f560c8c3c3008788bdf815dbe
martinjusto <aezakmi33@outlook.com>
2020-04-26 15:25:26 +0000

    Update script.py

--- Backups/script.py
+++ Backups/script.py
@@ -1,12 +1,25 @@
 import zipfile
 import os
+import hashlib
+import datetime
 
-def copia():
-    backup = zipfile.ZipFile('backups/fichero.zip', 'w') # Abrimos el fichero ZIP en modo escritura
-    backup.setpassword(b"password")# Establecemos contraseña en formato Bytes
-    backup.write('cosas', compress_type=zipfile.ZIP_DEFLATED) #Indicamos que debe comprimir
+
+def time():
+    x = datetime.datetime.now()
+    value = ("%s-%s-%s %s;%s" % (x.day, x.month, x.year, x.hour, x.second))
+    return value
+
+
+def copia(time):
+    backup = zipfile.ZipFile(rutabackups+cfile, 'w') # Abrimos el fichero ZIP en modo escritura
+    #backup.setpassword(b"password")# Establecemos contraseña en formato Bytes
+
+    for folder, subfolders, files in os.walk('cosas'):
+        for file in files:
+            backup.write(os.path.join(folder, file), os.path.relpath(os.path.join(folder, file), 'cosas'), compress_type=zipfile.ZIP_DEFLATED)
 
     backup.close() #Cerramos el fichero comprimido.
+
 
 def hash(archivo):
     hashmd5 = hashlib.md5()
@@ -17,6 +30,18 @@ def hash(archivo):
             hashsha.update(bloque)
     return hashmd5.hexdigest(), hashsha.hexdigest()  # Devolvemos el resultado
 
+
 def enviar():
     os.system("scp bsckups/fichero.zip USER@SERVER:PATH")
-    #falta añadir el login mediante clave
+    #falta añadir el login mediante claveosboxes.org
+
+
+rutabackups = 'backups/'
+cfile=(time()+'.zip') #Generamos el nombre del fichero comprimido.
+
+copia(cfile)
+md5,sha = hash(rutabackups+cfile)
+
+
+print('El hash MD5 es: ', md5)
+print('El hash SHA256 es: ', sha)
